2efdc15485b9a0a9c90bb925de2642e596b0916d
pitoco0dz9 <00001091104517sp@al.educacao.sp.gov.br>
2024-11-21 11:23:40 -0300

    pergunta

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matemática',
+    'resolva  2+5×2?',
+    '12'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília '
+)
+
+criaCartao(
+    'química',
+    'do que é formado a matéria?',
+    'Átomo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz qual sua cor favorita?',
+    'What is your favorite color?'
+)
